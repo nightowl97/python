ba0e4cfd0b9b1ffc61c20e66a3385ea9828d0997
NightOwl <feverier@hotmail.fr>
2017-03-07 21:18:15 +0000

    fix misspelling in numstoletters and add truncatable primes problem

--- euler/numstoletters.py
+++ euler/numstoletters.py
@@ -1,20 +1,22 @@
-import math
 
-dict = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
-        10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen',
-        17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'fourty', 50:'fifty',
-        60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety', 1000: 'thousand'}
+# Finally managed to get right answer because I fixed the misspelled 'forty'.
 
 
-def getlessthanhundred(i):
-    if i <= 20 or (i < 100 and i % 10 == 0):
-        return dict[i]
-    elif i < 100:
-        return dict[(i / 10) * 10] + dict[i % 10]    # Return length of 10s and unit
+dic = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
+       10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen',
+       17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty',
+       60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety', 1000: 'thousand'}
 
 
-def gethundreds(i):
-    return dict[i / 100] + 'hundred'
+def getlessthanhundred(n):
+    if n <= 20 or (n < 100 and n % 10 == 0):
+        return dic[n]
+    elif n < 100:
+        return dic[(n / 10) * 10] + dic[n % 10]    # Return length of 10s and unit
+
+
+def gethundreds(n):
+    return dic[n / 100] + 'hundred'
 
 
 def getlen(k):
